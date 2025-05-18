c0ef27d39399d6e167e687504b3e753d4dcd878f
olehzavalka <olehzavalka@gmail.com>
2025-05-18 23:24:33 +0200

    Rozbudowanie klasy Mapa.java oraz testu jednostkowego MapaTest.java

--- dostawa-towarow/src/main/java/dostawa/Mapa.java
+++ dostawa-towarow/src/main/java/dostawa/Mapa.java
@@ -1,4 +1,36 @@
 package dostawa;
 
+import java.util.List;
+
 public class Mapa {
+    private int szerokosc;
+    private int dlugosc;
+    private List<Magazyn> magazyny;
+    private List<PunktDostawy> punktyDostawy;
+
+
+    public Mapa(int szerokosc, int dlugosc, List<Magazyn> magazyny, List<PunktDostawy> punktyDostawy) {
+        this.szerokosc = szerokosc;
+        this.dlugosc = dlugosc;
+        this.magazyny = magazyny;
+        this.punktyDostawy = punktyDostawy;
+    }
+
+    // Gettery
+
+    public int getSzerokosc() {
+        return szerokosc;
+    }
+
+    public int getDlugosc() {
+        return dlugosc;
+    }
+
+    public List<Magazyn> getMagazyny() {
+        return magazyny;
+    }
+
+    public List<PunktDostawy> getPunktyDostawy() {
+        return punktyDostawy;
+    }
 }
--- dostawa-towarow/src/test/java/dostawa/MapaTest.java
+++ dostawa-towarow/src/test/java/dostawa/MapaTest.java
@@ -1,4 +1,18 @@
 package dostawa;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
 public class MapaTest {
+
+    @Test
+    public void testTworzenieMapy() {
+        Mapa mapa = new Mapa(30, 50, new ArrayList<>(), new ArrayList<>());
+
+        assertEquals(30, mapa.getSzerokosc());
+        assertEquals(50, mapa.getDlugosc());
+        assertNotNull(mapa.getMagazyny());
+        assertNotNull(mapa.getPunktyDostawy());
+    }
 }
