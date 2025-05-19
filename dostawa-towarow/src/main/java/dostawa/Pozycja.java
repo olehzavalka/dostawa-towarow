f200dc00d0f2d746c9cf61b795bc752635301b4d
olehzavalka <olehzavalka@gmail.com>
2025-05-19 13:34:50 +0200

    Rozbudowanie klasy Pozycja.java oraz testu jednostkowego PozycjaTest.java

--- dostawa-towarow/src/main/java/dostawa/Pozycja.java
+++ dostawa-towarow/src/main/java/dostawa/Pozycja.java
@@ -1,4 +1,19 @@
 package dostawa;
 
 public class Pozycja {
+    private int x;
+    private int y;
+
+    public Pozycja(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
--- dostawa-towarow/src/test/java/dostawa/PozycjaTest.java
+++ dostawa-towarow/src/test/java/dostawa/PozycjaTest.java
@@ -1,4 +1,14 @@
 package dostawa;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PozycjaTest {
+
+    @Test
+    public void testTworzeniePozycji() {
+        Pozycja pozycja = new Pozycja(14, 2);
+        assertEquals(14, pozycja.getX());
+        assertEquals(2, pozycja.getY());
+    }
 }
