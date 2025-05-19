3bfc0760d9459558f8e3ac83147f7535513a9adc
olehzavalka <olehzavalka@gmail.com>
2025-05-19 14:00:50 +0200

    Rozbudowanie klasy Magazyn.java oraz testu jednostkowego MagazynTest.java

--- dostawa-towarow/src/main/java/dostawa/Magazyn.java
+++ dostawa-towarow/src/main/java/dostawa/Magazyn.java
@@ -1,4 +1,19 @@
 package dostawa;
 
 public class Magazyn {
+    private int id;
+    private Pozycja pozycja;
+
+    public Magazyn(int id, Pozycja pozycja) {
+        this.id = id;
+        this.pozycja = pozycja;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Pozycja getPozycja() {
+        return pozycja;
+    }
 }
--- dostawa-towarow/src/test/java/dostawa/MagazynTest.java
+++ dostawa-towarow/src/test/java/dostawa/MagazynTest.java
@@ -1,4 +1,25 @@
 package dostawa;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MagazynTest {
+
+    @Test
+    public void testTworzenieMagazynow() {
+        Pozycja pozycja1 = new Pozycja(14, 4);
+        Magazyn magazyn1 = new Magazyn(1, pozycja1);
+        assertEquals(1, magazyn1.getId());
+        assertEquals(pozycja1, magazyn1.getPozycja());
+
+        Pozycja pozycja2 = new Pozycja(10, 2);
+        Magazyn magazyn2 = new Magazyn(2, pozycja2);
+        assertEquals(2, magazyn2.getId());
+        assertEquals(pozycja2, magazyn2.getPozycja());
+
+        Pozycja pozycja3 = new Pozycja(347, 399);
+        Magazyn magazyn3 = new Magazyn(145, pozycja3);
+        assertEquals(145, magazyn3.getId());
+        assertEquals(pozycja3, magazyn3.getPozycja());
+    }
 }
