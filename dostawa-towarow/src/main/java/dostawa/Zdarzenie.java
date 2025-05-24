86d675411e7f6ffc1413d5e76c640cf3c4a15ed6
olehzavalka <olehzavalka@gmail.com>
2025-05-24 22:49:36 +0200

    Rozbudowanie klasy Zdarzenie.java oraz klasy testowej ZdarzenieTest.java z testami jednostkowymi

--- dostawa-towarow/src/main/java/dostawa/Zdarzenie.java
+++ dostawa-towarow/src/main/java/dostawa/Zdarzenie.java
@@ -1,4 +1,15 @@
 package dostawa;
 
 public class Zdarzenie {
+    private final String rodzajZdarzenia;
+
+    public Zdarzenie(String rodzajZdarzenia) {
+        this.rodzajZdarzenia = rodzajZdarzenia;
+    }
+
+    // Gettery
+
+    public String getRodzajZdarzenia() {
+        return rodzajZdarzenia;
+    }
 }
--- dostawa-towarow/src/test/java/dostawa/ZdarzenieTest.java
+++ dostawa-towarow/src/test/java/dostawa/ZdarzenieTest.java
@@ -1,4 +1,23 @@
 package dostawa;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ZdarzenieTest {
+
+    @Test
+    public void testTworzenieZdarzenia() {
+        Zdarzenie zdarzenie = new Zdarzenie("awaria");
+        assertEquals("awaria", zdarzenie.getRodzajZdarzenia());
+    }
+
+    @Test
+    public void testRozneRodzajeZdarzen() {
+        Zdarzenie zdarzenie1 = new Zdarzenie("awaria");
+        Zdarzenie zdarzenie2 = new Zdarzenie("brak przejazdu");
+
+        assertEquals("awaria", zdarzenie1.getRodzajZdarzenia());
+        assertEquals("brak przejazdu", zdarzenie2.getRodzajZdarzenia());
+        assertNotEquals(zdarzenie1.getRodzajZdarzenia(), zdarzenie2.getRodzajZdarzenia());
+    }
 }
