56f265933d4bb4ff902abaa450b40c038c29160a
olehzavalka <olehzavalka@gmail.com>
2025-05-24 22:10:30 +0200

    Rozbudowanie klasy Pojazd.java oraz klasy testowej PojazdTest.java z testami jednostkowymi

--- dostawa-towarow/src/main/java/dostawa/Pojazd.java
+++ dostawa-towarow/src/main/java/dostawa/Pojazd.java
@@ -1,4 +1,39 @@
 package dostawa;
 
 public class Pojazd {
+    private int id;
+    private int ladownoscMax;
+    private int aktualnaIloscTowaru;
+    private Pozycja pozycja;
+
+    public Pojazd(int id, int ladownoscMax, Pozycja pozycja) {
+        this.id = id;
+        this.ladownoscMax = ladownoscMax;
+        this.pozycja = pozycja;
+        this.aktualnaIloscTowaru = 0;
+    }
+
+    // Gettery
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLadownoscMax() {
+        return ladownoscMax;
+    }
+
+    public int getAktualnaIloscTowaru() {
+        return aktualnaIloscTowaru;
+    }
+
+    public Pozycja getPozycja() {
+        return pozycja;
+    }
+
+    // Metody sprawdzajace
+
+    public boolean czyMoznaZaladowac(int ilosc) {
+        return (aktualnaIloscTowaru + ilosc) <= ladownoscMax;
+    }
 }
--- dostawa-towarow/src/test/java/dostawa/PojazdTest.java
+++ dostawa-towarow/src/test/java/dostawa/PojazdTest.java
@@ -1,4 +1,29 @@
 package dostawa;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PojazdTest {
+
+    @Test
+    public void testTworzeniePojazdu() {
+        Pozycja pozycja = new Pozycja(4, 5);
+        Pojazd pojazd = new Pojazd(7, 9, pozycja);
+
+        assertEquals(7, pojazd.getId());
+        assertEquals(9, pojazd.getLadownoscMax());
+        assertEquals(0, pojazd.getAktualnaIloscTowaru());
+        assertEquals(pozycja, pojazd.getPozycja());
+    }
+
+    @Test
+    public void testCzyMoznaZaladowac() {
+        Pozycja pozycja = new Pozycja(4, 5);
+        Pojazd pojazd = new Pojazd(4, 17, pozycja);
+
+        assertTrue(pojazd.czyMoznaZaladowac(5));
+        assertTrue(pojazd.czyMoznaZaladowac(17));
+        assertFalse(pojazd.czyMoznaZaladowac(18));
+        assertFalse(pojazd.czyMoznaZaladowac(34));
+    }
 }
