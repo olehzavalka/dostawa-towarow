9e4d08fc1f2a595d9e7460c33facf214bb0376fc
olehzavalka <olehzavalka@gmail.com>
2025-05-24 22:32:16 +0200

    Rozbudowanie klasy Towar.java oraz klasy testowej TowarTest.java z testami jednostkowymi

--- dostawa-towarow/src/main/java/dostawa/Towar.java
+++ dostawa-towarow/src/main/java/dostawa/Towar.java
@@ -1,4 +1,13 @@
 package dostawa;
 
 public class Towar {
+    private final String typ;
+
+    public Towar(String nazwa) {
+        this.typ = nazwa;
+    }
+
+    public String getNazwa() {
+        return typ;
+    }
 }
--- dostawa-towarow/src/test/java/dostawa/TowarTest.java
+++ dostawa-towarow/src/test/java/dostawa/TowarTest.java
@@ -1,4 +1,20 @@
 package dostawa;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TowarTest {
+
+    @Test
+    public void testTworzenieTowaru() {
+        Towar towar = new Towar("odziez");
+        assertEquals("odziez", towar.getNazwa());
+    }
+
+    @Test
+    public void testRozneNazwy() {
+        Towar towar1 = new Towar("elektronika");
+        Towar towar2 = new Towar("meble");
+        assertNotEquals(towar1.getNazwa(), towar2.getNazwa());
+    }
 }
