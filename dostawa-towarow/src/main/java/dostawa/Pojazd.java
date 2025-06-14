e4d51c8f17ec69c95ca36a057b1fb0095a1c70ee
olehzavalka <olehzavalka@gmail.com>
2025-06-14 23:31:00 +0200

    Dodanie obslugi stanow pojazdu

--- dostawa-towarow/src/main/java/dostawa/Pojazd.java
+++ dostawa-towarow/src/main/java/dostawa/Pojazd.java
@@ -1,5 +1,7 @@
 package dostawa;
 
+import java.util.*;
+
 public abstract class Pojazd {
     private int id;
     private int ladownoscMax;
@@ -13,6 +15,18 @@ public abstract class Pojazd {
         this.aktualnaIloscTowaru = 0;
     }
 
+    public static final int STAN_WOLNY = 0;
+    public static final int STAN_DOJEZDZA_DO_MAGAZYNU = 1;
+    public static final int STAN_LADOWANIE = 2;
+    public static final int STAN_DOJEZDZA_DO_PUNKTU = 3;
+    public static final int STAN_ROZLADUNEK = 4;
+
+    private int stanPojazdu = STAN_WOLNY;
+    private Magazyn magazynDocelowy;
+    private PunktDostawy punktDocelowy;
+    private int iloscDoDostarczenia;
+    private List<Pozycja> trasaDoCelu;
+
     // Settery
 
     public void setPozycja(Pozycja pozycja) {
@@ -21,6 +35,26 @@ public abstract class Pojazd {
 
     public void setAktualnaIloscTowaru(int ilosc) {
         this.aktualnaIloscTowaru = ilosc;
+    }
+
+    public void setStanPojazdu(int stan) {
+        this.stanPojazdu = stan;
+    }
+
+    public void setMagazynDocelowy(Magazyn magazynDocelowy) {
+        this.magazynDocelowy = magazynDocelowy;
+    }
+
+    public void setPunktDocelowy(PunktDostawy punktDocelowy) {
+        this.punktDocelowy = punktDocelowy;
+    }
+
+    public void setIloscDoDostarczenia(int ilosc) {
+        this.iloscDoDostarczenia = ilosc;
+    }
+
+    public void setTrasaDoCelu(List<Pozycja> trasa) {
+        this.trasaDoCelu = trasa;
     }
 
     // Gettery
@@ -39,6 +73,26 @@ public abstract class Pojazd {
 
     public Pozycja getPozycja() {
         return pozycja;
+    }
+
+    public int getStanPojazdu() {
+        return stanPojazdu;
+    }
+
+    public Magazyn getMagazynDocelowy() {
+        return magazynDocelowy;
+    }
+
+    public PunktDostawy getPunktDocelowy() {
+        return punktDocelowy;
+    }
+
+    public int getIloscDoDostarczenia() {
+        return iloscDoDostarczenia;
+    }
+
+    public List<Pozycja> getTrasaDoCelu() {
+        return trasaDoCelu;
     }
 
     // Metoda sprawdzajaca
