c9754475d9d37abcbdf288690e6ab6f681866ea4
olehzavalka <olehzavalka@gmail.com>
2025-06-15 11:26:05 +0200

    Pobranie od uzytkownika liczby epok w klasie Main.java, uruchamianie dzialania symulacji dla podanej liczby epok oraz wyswietlanie kazdej kolejnej epoki w konsoli

--- dostawa-towarow/src/main/java/dostawa/Main.java
+++ dostawa-towarow/src/main/java/dostawa/Main.java
@@ -1,6 +1,5 @@
 package dostawa;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +8,7 @@ public class Main {
 
         System.out.print("Podaj szerokosc mapy: ");
         int szerokosc = scanner.nextInt();
+
         System.out.print("Podaj dlugosc mapy: ");
         int dlugosc = scanner.nextInt();
 
@@ -27,41 +27,18 @@ public class Main {
         System.out.print("Podaj liczbe duzych pojazdow: ");
         int liczbaDuzych = scanner.nextInt();
 
-        Symulacja symulacja = new Symulacja(szerokosc, dlugosc, liczbaMagazynow, liczbaPunktowDostawy, liczbaMalych, liczbaSrednich, liczbaDuzych);
+        System.out.print("Podaj liczbe epok symulacji: ");
+        int liczbaEpok = scanner.nextInt();
+
+        Symulacja symulacja = new Symulacja(szerokosc, dlugosc, liczbaMagazynow, liczbaPunktowDostawy,
+                liczbaMalych, liczbaSrednich, liczbaDuzych
+        );
 
         symulacja.wyswietlMape();
         System.out.println(symulacja);
-        System.out.println("\n");
+        System.out.println();
 
-        // Wyswietlanie informacji o punktach dostawy
-        // symulacja.getMapa().wyswietlPunktyDostawy();
-
-        List<Zamowienie> zamowienia = symulacja.generujZamowienia();
-        for (Zamowienie zam : zamowienia) {
-            System.out.println("Punkt dostawy ID " + zam.getPunktDostawy().getId() +
-                    " (" + zam.getPunktDostawy().getPozycja().getX() + ", " + zam.getPunktDostawy().getPozycja().getY() + ")" +
-                    " zamawia " + zam.getIlosc() + " jednostek towaru");
-        }
-
-        for (Zamowienie zam : zamowienia) {
-            Magazyn magazyn = symulacja.znajdzNajblizszyMagazyn(zam.getPunktDostawy().getPozycja(), zam.getIlosc());
-            if (magazyn != null) {
-                Pojazd pojazd = magazyn.znajdzNajlepszyPojazd(symulacja.getPojazdy(), zam.getIlosc(), zam.getPunktDostawy().getPozycja());
-                if (pojazd != null) {
-                    Pozycja pozycjaPojazduPrzed = pojazd.getPozycja();
-                    //magazyn.ladujPojazdIMarsz(pojazd, magazyn, zam);
-                    System.out.println("Pojazd ID " + pojazd.getId() +
-                            " (" + pozycjaPojazduPrzed.getX() + ", " + pozycjaPojazduPrzed.getY() + ")" +
-                            " dostal zlecenie od magazynu ID " + magazyn.getId() +
-                            " (" + magazyn.getPozycja().getX() + ", " + magazyn.getPozycja().getY() + ")" +
-                            " i dostarczyl " + zam.getIlosc() + " jednostek towaru do punktu " +
-                            zam.getPunktDostawy().getId());
-                } else {
-                    System.out.println("Brak wolnego pojazdu dla zamowienia do punktu " + zam.getPunktDostawy().getId());
-                }
-            } else {
-                System.out.println("Nie znaleziono magazynu dla zamowienia do punktu " + zam.getPunktDostawy().getId());
-            }
-        }
+        // Uruchamianie symulacji
+        symulacja.uruchomSymulacje(liczbaEpok);
     }
 }
